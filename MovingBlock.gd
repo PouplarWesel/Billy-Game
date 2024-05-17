86da50f14964c6ac09c263ef7ce54b3ca7640b74
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2024-05-17 16:03:58 -0700

    A

--- MovingBlock.gd
+++ MovingBlock.gd
@@ -1,9 +1,11 @@
 extends StaticBody3D
 
-var isFoward = true
-var distance = 10
+var isFoward = false
+var distance = 3
 var timePause = 2
 var paused = false
+var flipped = false
+var isOn = true
 
 var count = 0
 
@@ -12,30 +14,47 @@ var count1 = 0
 var time
 
 func _physics_process(delta):
-	count1 += 1
-	if (isFoward && !paused && count1 % 20 == 0):
-		position.x += 1
-		count += 1
-		if ($Area3D.has_overlapping_bodies()):
-			var body = $Area3D.get_overlapping_bodies()
-			%Toad.position.x += 1
+	if (isOn):
+		count1 += 1
+		if (isFoward && !paused && count1 % 20 == 0):
+			if (flipped):
+				position.x += 1
+			else:
+				position.z += 1
+			count += 1
+			if ($Area3D.has_overlapping_bodies()):
+				var body = $Area3D.get_overlapping_bodies()
+				print(body)
+				if (flipped):
+					%Toad.position.x += 1
+				else:
+					%Toad.position.z += 1
+			
+				
+				
+		elif (!isFoward && !paused && count1 % 20 == 0):
+			if (flipped):
+				position.x += -1
+			else:
+				position.z += -1
+			count += 1
+			if ($Area3D.has_overlapping_bodies()):
+				var body = $Area3D.get_overlapping_bodies()
+				print(body)
+				if (flipped):
+					%Toad.position.x -= 1
+				else:
+					%Toad.position.z -= 1
+		
+		if (count == distance):
+			time = Time.get_unix_time_from_system()
+			count = 0
+			paused = true
+			
+		if (paused):
+			if (Time.get_unix_time_from_system() - time >= timePause):
+				paused = false
+				isFoward = !isFoward
+				
 		
 			
-			
-	elif (!isFoward && !paused && count1 % 20 == 0):
-		position.x -= 1
-		count += 1
-		%Toad.position.x -= 1
-	
-	if (count == distance):
-		time = Time.get_unix_time_from_system()
-		count = 0
-		paused = true
-		
-	if (paused):
-		if (Time.get_unix_time_from_system() - time >= timePause):
-			paused = false
-			isFoward = !isFoward
-			
-	
-		
